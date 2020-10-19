97db090954a6773a3990ed851e47dd14e43a330e
SpiderGamin <jaytaygame@gmail.com>
2020-10-19 15:58:55 -0700

    Small updates, mainly spelling fixes

--- README.md
+++ README.md
@@ -1,36 +1,28 @@
+**NOT MAINTAINED**  
+I may not come back to this project.  
+However if there are any bugs or issues, please open an issue on github.  
+Feel free to send pull requests.
+
 # phone-controler
-Use your phone to controll you pc.
+Use your phone to control you pc.
 
 Set the password by editing the `password` varuable in the `index.js` file.
 
-You can use `?pass=THE_PASSWORD` at the endo ofthe url to login without the password screen.
+You can use `?pass=THE_PASSWORD` at the end of the url to login without the password screen.
 
-# Fetures
-* [ ] Keyboard - almost complete
-* [ ] Trackpad - working progress
-  * Scroll
-  * Left/Right click
-  * Drag and drop
-* [ ] Game Controller
-  * Minecraft
-* [ ] Possible computer controlls
-  * Volume
-  * Power off
+## Updated
+Fixed a few things.
+
+# Features
+- Keyboard
 
 # Usage
-Install nodejs v12 or later
+Install nodejs v12 or later  
+Navigate to the project directory in the console and run `npm install`  
 
-Navigate to the project directory in the console,
+To start the program, run `npm start`  
 
-run `npm install`, 
+Go to the web browser on your phone and type `YOURPCIP:3000` replaceing YOURPCIP with your PC's ip address.  
 
-to start the program, run `npm start`.
-
-Go to you web browser on your phone and type `YOURPCIP:3000` replaceing YOURPCIP with your PC's ip address.
-
-To find your pcs IP address, open the command prompt and type `ipconfig`
-Find where it displays a number like this: `10.0.0.48`
-
-NOTE: This uses a node module `robotjs`, which may require you to install `node-gyp`
-
+NOTE: This uses a node module `robotjs`, which may require you to install `node-gyp`  
 To do so, run `npm install -g node-gyp` in the admin console (windows: win + x > Windows Powershell (admin)), then run `node-gyp rebuild`. Make sure to run `npm install` in the project directory again.
--- index.js
+++ index.js
@@ -1,101 +1,75 @@
-// The password is for security
 const password = 'PASSWORD';
 
 
-const express = require('express')
-const util = require('util')
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-const robot = require('robotjs')
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const robot = require('robotjs');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
-  if (request.query.pass === password) {
-    console.log('password right')
-    fs.readFile('auth/index.html', 'binary', function (err, file) {
-      if (err) {
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+	if (request.query.pass !== password) return;
+	fs.readFile('public/login.html', 'binary', function (err, file) {
+		if (err) {
+			response.writeHead(500, { "Content-Type": "text/plain" });
+			response.write(err + "\n");
+			response.end();
+			return;
+		}
 
-      var headers = {};
-      headers["Content-Type"] = "text/html";
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
-      return
-    })
-  } else {
-    fs.readFile('public/index.html', 'binary', function (err, file) {
-      if (err) {
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      var headers = {};
-      headers["Content-Type"] = "text/html";
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
-      return
-    })
-  }
+		var headers = {};
+		headers["Content-Type"] = "text/html";
+		response.writeHead(200, headers);
+		response.write(file, "binary");
+		response.end();
+		return;
+	});
 })
 
 app.post('/Password', (request, response) => {
-  if (request.body.password !== password) {
-    response.json({ message: 'Incorrect password' });
-    console.log('password wrong')
-  } else {
-    console.log('password right')
-    fs.readFile('auth/index.html', 'binary', function (err, file) {
-      if (err) {
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-      var headers = {};
-      headers["Content-Type"] = "text/html";
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
-      return;
-    })
-  }
+	if (request.body.password !== password) {
+		response.json({ message: 'Incorrect password' });
+	} else {
+		fs.readFile('public/index.html', 'binary', function (err, file) {
+			if (err) {
+				response.writeHead(500, { "Content-Type": "text/plain" });
+				response.write(err + "\n");
+				response.end();
+				return;
+			}
+			var headers = {};
+			headers["Content-Type"] = "text/html";
+			response.writeHead(200, headers);
+			response.write(file, "binary");
+			response.end();
+			return;
+		})
+	}
 })
 
 app.post('/keyboard', (request, response) => {
-  var log;
-  if (request.body.spec) {
-    if (request.body.character === 'enter') {
-      log = '\n'
-    } else if (request.body.character === 'space') {
-      log = ' '
-    }
-    robot.keyTap(request.body.character)
-  } else {
-    log = request.body.character;
-    robot.typeString(request.body.character)
-  }
-  response.writeHead(200);
-  response.write('gotcha');
-  response.end();
-  return;
+	let log;
+	if (request.body.spec) {
+		if (request.body.character === 'enter') {
+			log = '\n'
+		} else if (request.body.character === 'space') {
+			log = ' '
+		}
+		robot.keyTap(request.body.character)
+	} else {
+		log = request.body.character;
+		robot.typeString(request.body.character)
+	}
+	response.writeHead(200);
+	response.write('Um');
+	response.end();
+	return;
 })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+app.listen(port, () => {
+	console.log(`Controller started on port: ${port}`);
 })
